fa6ba856ee7a3b3c4c88d5451fe179ae0ba3b04e
Danail Gabenski <solidanski@gmail.com>
2018-07-30 04:54:15 -0600

    Update bootstrap.js
    Same changes to main bootstrap.js file

--- dist/js/bootstrap.js
+++ dist/js/bootstrap.js
@@ -1501,7 +1501,7 @@ if (typeof jQuery === 'undefined') {
 
       var complete = function () {
         var prevHoverState = that.hoverState
-        that.$element.trigger('shown.bs.' + that.type)
+        null != that.$element && that.$element.trigger('shown.bs.' + that.type)
         that.hoverState = null
 
         if (prevHoverState == 'out') that.leave(that)
@@ -1745,7 +1745,7 @@ if (typeof jQuery === 'undefined') {
     var that = this
     clearTimeout(this.timeout)
     this.hide(function () {
-      that.$element.off('.' + that.type).removeData('bs.' + that.type)
+      null != that.$element && that.$element.off('.' + that.type).removeData('bs.' + that.type)
       if (that.$tip) {
         that.$tip.detach()
       }
